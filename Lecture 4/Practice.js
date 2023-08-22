3da4f1b993aa4cb555f27c9dcc1716fa27313186
unknownhn <140600568+unknownhn@users.noreply.github.com>
2023-08-22 12:22:40 +0500

    Create Practice.js

---
+++ Lecture 4/Practice.js
@@ -0,0 +1,151 @@
+//string methods
+// let names='Hello World'
+// console.log(names[0]);
+
+///Method at
+// let names='hello world'
+// console.log(names.at(-1));
+
+///Method CharAt
+// let names='hello world'
+// console.log(names.at(-1));
+
+//Method concat
+// let names='Bye world'
+// let names2='(false)'
+// console.log(names.concat(" ",names2));
+
+//Method replace
+// let names='hello world bye'
+// console.log(names.replace("hello","privet"));
+
+//Method split
+// let names='My name is unonimus'
+// console.log(names.split(" "));
+
+//Method substring  (и метод -1... намегира)
+// let names="Heloo World anonimus"
+// console.log(names.substring(0,2));
+
+//Method slice(start , end)
+// let names="Heloo World anonimus"
+// console.log(names.slice(-5,1));
+
+//Method toLowerCase
+// let names="Heloo World anonimus"
+// console.log(names.toLowerCase());
+//Method toUpperrCase
+// let names="Heloo World anonimus"
+// console.log(names.toLowerCase());
+
+//Method trim  
+// let names=" Heloo World anonimus "
+// console.log(names.trim());
+
+//Method includes
+// let names=" Heloo World anonimus "
+// console.log(names.includes("ano"));
+
+//Method )(toString)
+// let names='2'
+// let src=Number(names)
+// console.log(typeof src);
+
+//Method indexOf
+// let names="Hello world of fairy"
+// console.log(names.indexOf("w"));
+
+//Method repeat
+// let names="Hello world of fairy "
+// console.log(names.repeat(2));
+
+
+
+///Number
+//Method Math.mix(1,2,3,4,5,6)
+
+///Method Math.min(-1,3,2,0)
+
+///Method Math.sqrt
+
+///Method Math.abs
+
+///Method Math.random()*10 (from 1 to 10)
+
+///Method Math.isNaN 
+// let names= 2 * "2a"
+// console.log(isNaN(names));
+
+//Array Method
+//Method push
+// let arr=[1,2,3,4,5]
+// let res=arr.push(5,"hi")
+// console.log(res);
+
+//Method pop
+// let arr=[1,2,3,4,5]
+// let res=arr.pop(5)
+// console.log(arr);
+
+//Method unshift
+// let arr=[1,2,3,4,5]
+// let res=arr.unshift(0,0,2)
+// console.log(arr);
+
+//Method tostring
+// let arr=[1,2,3,4,5]
+// let res=arr.toString()
+// console.log(res);
+
+//Method join  (only array method)
+// let arr=[1,2,3,4,5]
+// let res=arr.join(" ")
+// console.log(res);
+
+// Method indexOf
+// let arr=[1,2,3,4,5]
+// let res=arr.indexOf(" ")
+// console.log(res);
+
+//Method slice
+// let arr=[1,2,3,4,5]
+// let res=arr.indexOf(" ")
+// console.log(res);
+
+//Method concat
+// let arr=[1,2,3,4,5]
+// let res=arr.concat(" ")
+// console.log(res);
+
+//Method splice  (which delet) and input
+// let arr=[1,2,3,4,5]
+// let res=arr.splice(1,2)
+// console.log(arr);
+
+///////////////////////////////////////////
+//Method forEach
+// let arr=[1,2,3,4,5]
+// arr.forEach(function(e,i,arr){
+//   console.log(e,i,arr);
+// });
+
+//Method map
+// let arr=[1,2,3,4,5]
+// let res=arr.map(function(e,i,arr){
+//   return e*2
+// });
+// console.log(res);
+
+//Method find
+// let arr=[1,2,3,4,5]
+// let res=arr.find(function(e,i,arr){
+//   return e*2
+// });
+// console.log(res);
+
+//Method filter
+// let arr=[1,2,3,4,5]
+// let res=arr.filter(function(e,i,arr){
+//   return e%2==0
+// });
+// console.log(res);
